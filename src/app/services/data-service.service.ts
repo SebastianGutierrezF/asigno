8f24dffe0e705ed5ee9307ecd3997298199423af
Sebastian Gutierrez <l.gufs980514@itses.edu.mx>
2022-10-13 13:48:28 -0500

    Pegarle a servicios en heroku

--- src/app/services/data-service.service.ts
+++ src/app/services/data-service.service.ts
@@ -6,7 +6,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
   providedIn: 'root'
 })
 export class DataService {
-  baseUrl = 'http://localhost/asignowebservice/controller/';
+  baseUrl = 'https://asignowebservice.herokuapp.com/controller/';
   public httpOptions = {
     headers: new HttpHeaders({
       'Content-Type': 'application/json; charset=utf-8',
