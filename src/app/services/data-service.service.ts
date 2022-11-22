7e09c28dfdb6c2899fb9e48b19ab456f7f80d831
Sebastian Gutierrez <l.gufs980514@itses.edu.mx>
2022-11-22 08:45:57 -0600

    Incrementar limite para build

--- src/app/services/data-service.service.ts
+++ src/app/services/data-service.service.ts
@@ -7,16 +7,15 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 })
 export class DataService {
   // Cambiar a remoto asignowebservice.herokuapp.com
-  baseUrl = 'http://asignowebservice.herokuapp.com/controller/';
+  baseUrl = 'https://asigno.herokuapp.com/controller/';
   // baseUrl = 'http://localhost/asignowebservice/controller/';
-  public httpOptions = {
-    headers: new HttpHeaders({
-      'Content-Type': 'application/json; charset=utf-8',
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': 'GET, PUT, POST, DELETE, OPTIONS',
-      'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-    })
-  };
+  // public options = {
+  //   headers: new HttpHeaders({
+  //     'Content-Type': 'application/json; charset=utf-8',
+  //     'Access-Control-Request-Methods': 'GET, PUT, POST, DELETE, OPTIONS',
+  //     'Access-Control-Request-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  //   })
+  // };
 
   constructor(private http: HttpClient) { }
 
@@ -25,6 +24,6 @@ export class DataService {
   }
 
   post(model: string, action: string, datos: any) {
-    return this.http.post(`${this.baseUrl}${model}.php?option=${action}`, datos, this.httpOptions);
+    return this.http.post(`${this.baseUrl}${model}.php?option=${action}`, datos);
   }
 }
